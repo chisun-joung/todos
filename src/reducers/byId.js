42ee0e2bd4ac17ff2103d09439070ee02dad5d12
chisun-joung <csjoung@gmail.com>
2017-02-28 10:01:25 +0900

    ....

--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -1,7 +1,7 @@
 const byId = (state = {}, action) => {
     switch (action.type) {
         case 'RECEIVE_TODOS':
-            const nextState = {...state};
+            const nextState = { ...state };
             action.response.forEach(todo => {
                 nextState[todo.id] = todo;
             });
